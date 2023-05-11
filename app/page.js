ef5e7bccb0b1c32cc13bb0974e6da8637ace33c2
Hirriof <Jerryhirriof@gmail.com>
2023-05-11 10:42:40 +0200

    qsdqsdqsdqs

--- app/page.js
+++ app/page.js
@@ -22,57 +22,15 @@ export default function Home() {
   }
 
   return (
-    <form className="container" onSubmit={handleSubmit}>
-      <h1>Get in touch</h1>
-      <div className="email block">
-        <label htmlFor="frm-email">Email</label>
-        <input
-          id="frm-email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          required
-        />
-      </div>
-      <div className="block phone">
-        <label htmlFor="frm-phone">Phone</label>
-        <input
-          id="frm-phone"
-          type="tel"
-          name="phone"
-          autoComplete="tel"
-          required
-        />
-      </div>
-      <div className="name block">
-        <div>
-          <label htmlFor="frm-first">First Name</label>
-          <input
-            id="frm-first"
-            type="text"
-            name="name"
-            autoComplete="given-name"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="frm-last">Last Name</label>
-          <input
-            id="frm-last"
-            type="text"
-            name="last"
-            autoComplete="family-name"
-            required
-          />
-        </div>
-      </div>
-      <div className="message block">
-        <label htmlFor="frm-message">Message</label>
-        <textarea id="frm-message" rows="6" name="message"></textarea>
-      </div>
-      <div className="button block">
+    <form onSubmit={handleSubmit}>
+
+
+
+
+
+
         <button type="submit">Submit</button>
-      </div>
+
     </form>
   );
 }
