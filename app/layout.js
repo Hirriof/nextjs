a89a9557215948b666a6bc5ee30a91de8e408475
Hirriof <Jerryhirriof@gmail.com>
2023-05-11 16:50:55 +0200

    qsdqsd

--- app/layout.js
+++ app/layout.js
@@ -26,22 +26,22 @@ export default function RootLayout({ children }) {
         <meta name='description' content='SpaceRent : économisez sur vos séjours et revendez-les si vos plans changent !'/>
         <meta name='author' content='SpaceRent' />
 
-    {/*<!-- jQuery -->*/}
-    <Script src="vendor/jquery/jquery.min.js"></Script>
+        {/*<!-- jQuery -->*/}
+        <Script src="vendor/jquery/jquery.min.js"></Script>
 
-    {/*<!-- Bootstrap Core JavaScript -->*/}
-    <Script src="vendor/bootstrap/js/bootstrap.min.js"></Script>
-	<Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js"></Script>
-    
-    {/*<!-- Wow Animations -->*/}
-    <Script src="vendor/wow.min.js"></Script>
+        {/*<!-- Bootstrap Core JavaScript -->*/}
+        <Script src="vendor/bootstrap/js/bootstrap.min.js"></Script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js"></Script>
+        
+        {/*<!-- Wow Animations -->*/}
+        <Script src="vendor/wow.min.js"></Script>
 
-    {/*<!-- Plugin JavaScript -->*/}
-    <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></Script>
+        {/*<!-- Plugin JavaScript -->*/}
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></Script>
 
-    {/*<!-- Theme JavaScript -->*/}*
-    <Script src="js/main.js"></Script>
-    <Script src="js/jquery.isotope.js"></Script>
+        {/*<!-- Theme JavaScript -->*/}
+        <Script src="js/main.js"></Script>
+        <Script src="js/jquery.isotope.js"></Script>
 
 
       </head>
